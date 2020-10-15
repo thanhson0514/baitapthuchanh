f53d9c63d2ccd66569f88df0ab04f0a6d56a3297
thanhson0514 <thanhson0514@gmail.com>
2020-10-15 22:35:27 +0700

    complete Phan_1

--- Phan_2/bai_4/bai4.cpp
+++ Phan_2/bai_4/bai4.cpp
@@ -1,32 +1,132 @@
-#include<iostrem>
+#include<iostream>
 using namespace std;
 
 class Vector {
 private:
     int n, *v;
 public:
+    // biến đếm static
+    static int count;
     // hàm tạo
-    Vector(){}
+    Vector(){
+        this->n = 0;
+        ++count;
+    }
+
+    // hàm sao chép
+    Vector(const Vector &a) {
+        this->n = a.n;
+        this->v = a.v;
+        ++count;
+    }
+
     Vector(int size, int *a){
         n = size;
         v = new int[n];
         for (int i = 0; i < n; ++i)
             v[i] = a[i];
-    }
 
-    // hàm sao chép
-    Vector(Vector a) {
-        this->n = a.n;
-        this->v = a.v;
+        ++count;
     }
 
     //hàm hủy
     ~Vector(){}
+
+    // toán tử gán
+    Vector operator=(Vector &b) {
+        Vector c;
+        c.v = b.v;
+        c.n = b.n;
+        return c;
+    }
+
+    // toán tử so sánh bằng
+    bool operator==(Vector &b) {
+        int i = 0, j = 0;
+        for (;i < n; ++i) {
+            if (v[i] > b.v[j]) return false;
+            else if (v[i] < b.v[j]) return false;
+            if (j < b.n) ++j;
+        }
+        return i != j ? false:true;
+    }
+
+    // toán tử so sánh khác
+    bool operator!=(Vector &b) {
+        int i = 0, j = 0;
+        for (;i < n; ++i) {
+            if (v[i] > b.v[j]) return true;
+            else if (v[i] < b.v[j]) return true;
+            if (j < b.n) ++j;
+        }
+        return i == j ? false:true;
+    }
+
+    // toán tử lấy phần tử tại chỉ số
+    int operator[](int i) {
+        if (i > n || i < 0) return 0;
+        return v[i];
+    }
+
+    // toán tử nhập
+    friend istream &operator>>(istream &in, Vector &V) {
+        cout << "Nhap so phan tu: "; in >> V.n;
+        V.v = new int[V.n];
+        cout << "Nhap cac phan tu: ";
+        for (int i = 0; i < V.n; ++i) {
+            in >> *(V.v+i);
+        }
+
+        return in;
+    }
+
+    // toán tử xuất
+    friend ostream &operator<<(ostream &out, Vector &V) {
+        cout << "So phan tu: " << V.n << endl;
+        cout << "Cac phan tu:";
+        for (int i = 0; i < V.n; ++i)
+            out <<  " " << V.v[i];
+
+        return out;
+    }
 };
-
+int Vector::count = 0;
 int main() {
+    Vector V1;
+    cin >> V1;
+    cout << "V: " << V1 << endl;
 
+    cout << "----------------------------------\n";
+    int arr[] = {1,2,3,4};
+    Vector V2(4,arr);
+    cout << "V2: " << V2 << endl;
 
+    cout << "----------------------------------\n";
+    Vector V3(V1);
+    cout << "V3: " << V3 << endl;
+
+    cout << "----------------------------------\n";
+    cout << "V1 != V2 is ";
+    if (V1 != V2) cout << "true" << endl;
+    else cout << "false" << endl;
+
+    cout << "V1 == V3 is ";
+    if (V1 == V3) cout << "true"<< endl;
+    else cout << "false" << endl;
+
+    cout << "V3 == V2 is ";
+    if (V3 == V2) cout << "true" << endl;
+    else cout << "false" << endl;
+
+    cout << "----------------------------------\n";
+    int i = 0;
+    cout << "Nhap phan tu thu: "; cin >> i;
+    cout << "Phan tu thu " << i << " cua V2: " << V2[i] << endl;
+
+    cout << "----------------------------------\n";
+    Vector V4 = V2;
+    cout << "V4: " << V4 << endl;
+    cout << "So vector da khoi tao: " << Vector::count << endl;
     return 0;
 }
 
--- Phan_2/bai_5/bai5.cpp
+++ Phan_2/bai_5/bai5.cpp
@@ -1,28 +1,166 @@
-#include<iostrem>
+#include<iostream>
+#include<string.h>
+#define N 10000
 using namespace std;
 
 class String {
 private:
-    char s;
-    int length;
+    int length = 0;
+    char *s;
 public:
     // hàm tạo
     String(char *s="") {
         this->s = s;
+        int i = 0;
+        while ( *(this->s+i) != '\0')
+        {
+            ++this->length;
+            ++i;
+        }
     }
 
     // hàm sao chép
-    String(String s2) {
+    String(const String &s2) {
         this->s = s2.s;
         this->length = s2.length;
     }
 
     // hàm hủy
     ~String(){}
+
+    // toán tử gán
+    String &operator=(String &b) {
+        this->s = b.s;
+        this->length = b.length;
+        return *this;
+    }
+
+    // toán tử cộng
+    String &operator+=(const String &b) {
+        int n = this->length + b.length;
+        char *buf = new char[n];
+        strcpy(buf, this->s);
+        strcat(buf,b.s);
+
+        this->s = buf;
+        this->length = n;
+        return *this;
+    }
+
+    // toán tử so sánh bằng
+    bool operator==(String &b) {
+        int i = 0, j = 0;
+        if (length != b.length) return false;
+        for (; i < length; ++i) {
+            if (*(this->s+i) != b.s[j]) return false;
+            ++j;
+        }
+        return true;
+    }
+
+    // toán tử so sánh khác
+    bool operator!=(String &b) {
+        int i = 0, j = 0;
+        if (length != b.length) return true;
+        for (; i < length; ++i) {
+            if (*(this->s+i) != b.s[j]) return true;
+            ++j;
+        }
+        return false;
+    }
+
+    // toán tử so sánh lớn hơn
+    bool operator>(String &b) {
+        int i = 0, j = 0;
+        for (; i < length; ++i) {
+            if (*(this->s+i) > b.s[j]) return true;
+            else if (*(this->s+i) < b.s[j]) return false;
+            if(j < b.length) ++j;
+        }
+        return length > b.length ? true: false;
+    }
+
+    // toán tử so sánh bé hơn
+    bool operator<(String &b) {
+        int i = 0, j = 0;
+        for (; i < length; ++i) {
+            if (*(this->s+i) > b.s[j]) return false;
+            else if (*(this->s+i) < b.s[j]) return true;
+            if(j < b.length) ++j;
+        }
+        return length > b.length ? false: true;
+    }
+
+    // toán tử so sánh lớn hơn hoặc bằng
+    bool operator>=(String &b) {
+        int i = 0, j = 0;
+        for (; i < length; ++i) {
+            if (*(this->s+i) > b.s[j]) return true;
+            else if (*(this->s+i) < b.s[j]) return false;
+            if(j < b.length) ++j;
+        }
+        return length >= b.length ? true: false;
+    }
+
+    // toán tử nhỏ hơn hoặc bằng
+    bool operator<=(String &b) {
+        int i = 0, j = 0;
+        for (; i < length; ++i) {
+            if (*(this->s+i) > b.s[j]) return false;
+            else if (*(this->s+i) < b.s[j]) return true;
+            if(j < b.length) ++j;
+        }
+        return length >= b.length ? false: true;
+    }
+
+    // toán tử lấy phần tử xâu tại 1 chỉ số
+    char operator[](int i) {
+        if (i < 0 || i > length) return 0;
+        return s[i];
+    }
+
+    // toán tử nhập
+    friend istream &operator>>(istream &in, String &S) {
+        char *new_s = new char[N];
+        cout << "Nhap xau:"; in >> new_s;
+        S.s = new_s;
+        int i = 0;
+        while ( *(S.s+i) != '\0' ) {
+            ++S.length;
+            ++i;
+        }
+        return in;
+    }
+
+    // toán tử nhập
+    friend ostream &operator<<(ostream &out, String &S) {
+        out << "Do dai cua chuoi la: " << S.length << endl;
+        out << "Chuoi co dang: " << S.s << endl;
+        return out;
+    }
 };
 
 int main() {
+    String s1;
+    cin >> s1;
+    cout << s1;
+    cout << "----------------------------------\n";
+    String s2("Hello World");
+    s2 += s1;
+    cout << s2;
 
+    cout << "----------------------------------\n";
+    String s3 = s1;
+    cout << "s3 == s2: ";
+    if (s3 == s2) cout << "True" << endl;
+    else cout << "False" << endl;
 
+    cout << "s3 >= s1: ";
+    if (s3 >= s1) cout << "True" << endl;
+    else cout << "False" << endl;
+
+    cout << "----------------------------------\n";
+    int i; cout << "Nhap phan tu thu i: "; cin>>i;
+    cout << "Phan tu thu " << i << " cua s1: " << s1[i] << endl;
     return 0;
 }
